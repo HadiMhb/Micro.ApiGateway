8e5faf5c8e33ebbcf663d217164e9d9be5e57f31
h.mohebi <h.mohebi@MOHEBI-PC>
2024-02-29 20:37:47 +0330

    Add Ocelot configurations

--- Micro.ApiGateway/Program.cs
+++ Micro.ApiGateway/Program.cs
@@ -1,20 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("appsettings.json", true, true)
+        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+        .AddJsonFile("ocelot.json")
+        .AddEnvironmentVariables();
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseOcelot().Wait();
 
 app.UseHttpsRedirection();
 
